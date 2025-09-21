63650997da1cdf5a9feb68455d19efa55b0bbd88
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2025-09-21 13:20:40 +0200

    strip useless code

--- startup.sh
+++ startup.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-_term() { 
+terminate() { 
   echo "Caught SIGTERM signal! In BASH" 
   kill -TERM "$pid_avahi" 2>/dev/null # kill the avahi
   sleep 1
-  kill -TERM "$pid_dbus" 2>/dev/null # kill the avahi
+  kill -TERM "$pid_dbus" 2>/dev/null # kill the dbus
 }
 
 echo Starting D-BUS
-/usr/bin/dbus-daemon --fork --nopidfile --nosyslog --print-address=1 --system >/tmp/ad
+/usr/bin/dbus-daemon --fork --nopidfile --nosyslog --system
 pid_dbus=$!
-sleep 1
-
-DBUS_SESSION_BUS_ADDRESS=$(cat /tmp/ad)
-
-echo DBUS=$DBUS_SESSION_BUS_ADDRESS
 
 echo Starting avahi
 /sbin/avahi-daemon &
 pid_avahi=$!
 
-
-trap _term SIGTERM
+trap terminate SIGTERM
 
 /helper/bin/python3 /helper/dockersock_watcher.py
-
-#wait "$child"
